d386f5c88a4a9d4bd401951819a94d77a9c17052
Renata Pulz <84743054+renatapulz@users.noreply.github.com>
2021-10-23 02:26:36 -0300

    aula 4

---
+++ main.js
@@ -0,0 +1,32 @@
+const socialMedia = {
+  github: 'renatapulz',
+  youtube: 'rocketseat',
+  facebook: 'rocketseat',
+  instagram: 'rocketseat_oficial',
+  twitter: 'rocketseat'
+}
+
+function changeSocialMedia() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${socialMedia[social]}`
+  }
+}
+
+changeSocialMedia()
+
+function getInfos() {
+  const url = `https://api.github.com/users/${socialMedia.github}`
+
+  //fetch= bate na url e pega a resposta / .then = promessa
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userAvatar.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getInfos()
